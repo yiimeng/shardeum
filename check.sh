58091572fe95975222476a4906206b2248808db8
yiimeng <77013305+yiimeng@users.noreply.github.com>
2023-04-01 15:20:27 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 while :
 do
-docker exec shardeum-dashboard operator-cli status | grep -E "stopped|standby"
-if [ $? -eq 0 ]
-then
+    docker exec shardeum-dashboard operator-cli status | grep -E "stopped|state: null"
+    if [ $? -eq 0 ]
+    then
         echo $(date)
         echo "START SHARDEUM"
         docker exec shardeum-dashboard operator-cli start
-fi
-sleep 1m
+    fi
+    sleep 1m
 done
